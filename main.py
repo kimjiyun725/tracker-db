f987ca92b8cfbd3319211a31e819aa1c84a6133e
kimjiyun725 <kimjiyun725@gmail.com>
2022-06-27 18:41:13 -0400

    Manager and Salesperson Cross References Properly

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from concurrent.futures.process import _python_exit
-from ctypes import addressof
 from flask import Flask, jsonify, request
 import json
 import database.accessor
@@ -8,8 +6,10 @@ import uuid
 
 app = Flask(__name__)
 
-# API Routes
-@app.route('/products',methods=['GET', 'POST'])
+# PRODUCTS API ROUTES
+
+
+@app.route('/products', methods=['GET', 'POST'])
 def products():
     global id_
     global name_
@@ -22,32 +22,26 @@ def products():
     if request.method == 'POST':
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
-        for row in request_data:
-            id_ = row['id']
-            name_ = row['name']
-            manufacturer = row['manufacturer']
-            style = row['style']
-            purchase_price = row['purchase_price']
-            quantity = row['quantity']
-            commission_pct = row['commission_pct']
+        id_ = request_data['id']
+        name_ = request_data['name']
+        manufacturer = request_data['manufacturer']
+        style = request_data['style']
+        purchase_price = request_data['purchase_price']
+        quantity = request_data['quantity']
+        commission_pct = request_data['commission_pct']
 
         conn = sqlite3.connect('bespoked.db')
         conn.row_factory = sqlite3.Row
         cursor = conn.cursor()
 
-        success = database.accessor.insert_product(cursor, id_, name_, manufacturer, style, purchase_price, quantity, commission_pct)
-
+        success = database.accessor.insert_product(
+            cursor, id_, name_, manufacturer, style, purchase_price, quantity, commission_pct)
         print(f"{success}")
-
-        rows = database.accessor.get_products(cursor)
-        print("Print out input:")
-        for row in rows:
-            print([row["id"], row["name"], row["manufacturer"], row["style"], row["purchase_price"], row["quantity"], row["commission_pct"]])
 
         conn.commit()
         cursor.close()
         conn.close()
-        
+
         return " "
 
     elif request.method == 'GET':
@@ -60,16 +54,16 @@ def products():
         for row in rows:
             list_of_products.append(
                 {
-                "id": row["id"], 
-                "name": row["name"], 
-                "manufacturer": row["manufacturer"], 
-                "style": row["style"], 
-                "purchase_price": row["purchase_price"], 
-                "quantity": row["quantity"], 
-                "commission_pct": row["commission_pct"]
+                    "id": row["id"],
+                    "name": row["name"],
+                    "manufacturer": row["manufacturer"],
+                    "style": row["style"],
+                    "purchase_price": row["purchase_price"],
+                    "quantity": row["quantity"],
+                    "commission_pct": row["commission_pct"]
                 }
             )
-        print(list_of_products)
+
         conn.commit()
         cursor.close()
         conn.close()
@@ -77,13 +71,16 @@ def products():
 
     return " "
 
-@app.route('/salespersons', methods=['GET','POST'])
+# SALESPERSONS API ROUTES
+
+
+@app.route('/salespersons', methods=['GET', 'POST'])
 def salespersons():
     global id_
     global first_name
     global last_name
     global address
-    global phone 
+    global phone
     global start_date
     global termination_date
     global manager
@@ -91,41 +88,35 @@ def salespersons():
     if request.method == 'POST':
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
-        for row in request_data:
-            id_ = row['id']
-            first_name = row['first_name']
-            last_name = row['last_name']
-            address = row['address']
-            phone = row['phone']
-            start_date = row['start_date']
-            termination_date = row['termination_date']
-            manager = row['manager']
+        id_ = request_data['id']
+        first_name = request_data['first_name']
+        last_name = request_data['last_name']
+        address = request_data['address']
+        phone = request_data['phone']
+        start_date = request_data['start_date']
+        termination_date = request_data['termination_date']
+        manager = request_data['manager']
 
         conn = sqlite3.connect('bespoked.db')
         conn.row_factory = sqlite3.Row
         cursor = conn.cursor()
-        checking = database.accessor.check_managers(cursor, manager)
+
+        checking = database.accessor.insert_manager_salespersons(
+            cursor, id_, manager)
         if checking == False:
             conn.commit()
             cursor.close()
             conn.close()
             return " "
 
-        conn = sqlite3.connect('bespoked.db')
-        conn.row_factory = sqlite3.Row
-        cursor = conn.cursor()
-
-        success = database.accessor.insert_salesperson(cursor, id_, first_name, last_name, address, phone, start_date, termination_date)
-
+        success = database.accessor.insert_salesperson(
+            cursor, id_, first_name, last_name, address, phone, start_date, termination_date)
         print(f"{success}")
 
-        rows = database.accessor.get_salespersons(cursor)
-        print("Print out input:")
-        
         conn.commit()
         cursor.close()
         conn.close()
-        
+
         return " "
 
     elif request.method == 'GET':
@@ -133,27 +124,43 @@ def salespersons():
         conn.row_factory = sqlite3.Row
         cursor = conn.cursor()
 
-        rows = database.accessor.get_salespersons(cursor)
+        manager_salespersons = database.accessor.get_manager_salespersons(
+            cursor)
+        managers = database.accessor.get_managers(cursor)
+        salespersons = database.accessor.get_salespersons(cursor)
+
         list_of_salespersons = []
-        for row in rows:
-            list_of_salespersons.append(
-                {
-                "id": row["id"], 
-                "first_name": row["first_name"], 
-                "last_name": row["last_name"], 
-                "address": row["address"], 
-                "phone": row["phone"], 
-                "start_date": row["start_date"], 
-                "termination_date": row["termination_date"], 
-                }
-            )
-        print(list_of_salespersons)
+        for id_ in manager_salespersons:
+            for salesperson in salespersons:
+                if id_['salesperson_id'] == salesperson['id']:
+                    desired_manager = ''
+                    for manager in managers:
+                        if id_['manager_id'] == manager['id']:
+                            desired_manager = manager['first_name'] + \
+                                " " + manager['last_name']
+
+                    list_of_salespersons.append(
+                        {
+                            "id": salesperson["id"],
+                            "first_name": salesperson["first_name"],
+                            "last_name": salesperson["last_name"],
+                            "address": salesperson["address"],
+                            "phone": salesperson["phone"],
+                            "start_date": salesperson["start_date"],
+                            "termination_date": salesperson["termination_date"],
+                            "manager": desired_manager
+                        }
+                    )
+
         conn.commit()
         cursor.close()
         conn.close()
         return jsonify(list_of_salespersons)
-    
-@app.route('/managers', methods=['GET','POST'])
+
+# MANAGERS API ROUTE
+
+
+@app.route('/managers', methods=['GET', 'POST'])
 def managers():
     global id_
     global first_name
@@ -162,26 +169,22 @@ def managers():
     if request.method == 'POST':
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
-        for row in request_data:
-            id_ = row['id']
-            first_name = row['first_name']
-            last_name = row['last_name']
+        id_ = request_data['id']
+        first_name = request_data['first_name']
+        last_name = request_data['last_name']
 
         conn = sqlite3.connect('bespoked.db')
         conn.row_factory = sqlite3.Row
         cursor = conn.cursor()
 
-        success = database.accessor.insert_manager(cursor, id_, first_name, last_name)
-
+        success = database.accessor.insert_manager(
+            cursor, id_, first_name, last_name)
         print(f"{success}")
 
-        rows = database.accessor.get_managers(cursor)
-        print("Print out input:")
-        
         conn.commit()
         cursor.close()
         conn.close()
-        
+
         return " "
 
     elif request.method == 'GET':
@@ -194,12 +197,12 @@ def managers():
         for row in rows:
             list_of_managers.append(
                 {
-                "id": row["id"], 
-                "first_name": row["first_name"], 
-                "last_name": row["last_name"], 
+                    "id": row["id"],
+                    "first_name": row["first_name"],
+                    "last_name": row["last_name"],
                 }
             )
-        print(list_of_managers)
+
         conn.commit()
         cursor.close()
         conn.close()
--- sample.py
+++ sample.py
@@ -1,3 +1,4 @@
+from os import access
 import database.accessor
 import sqlite3
 import uuid
@@ -7,26 +8,66 @@ conn = sqlite3.connect('bespoked.db')
 conn.row_factory = sqlite3.Row
 cursor = conn.cursor()
 
-#PRODUCT SAMPLE
+# PRODUCT SAMPLE
 # -------------------------------------------------------------------
-database.accessor.insert_product(cursor, str(uuid.uuid4()), 'Verve', 'Trek Bikes', 'Electric', 3300.00, 15, 10)
-database.accessor.insert_product(cursor, str(uuid.uuid4()), 'Slash', 'Trek Bikes', 'Trail', 12549.99, 5, 15)
-database.accessor.insert_product(cursor, str(uuid.uuid4()), 'Propel Advanced', 'Giant Bicycle', 'Racing', 5200.00, 15, 10)
-database.accessor.insert_product(cursor, str(uuid.uuid4()), 'Defy Advanced', 'Giant Bicycle', 'Endurance', 6300.00, 10, 10)
-database.accessor.insert_product(cursor, str(uuid.uuid4()), 'RL 275', 'Redline', 'Big', 850.00, 20, 8)
-database.accessor.insert_product(cursor, str(uuid.uuid4()), 'Asset 24', 'Redline', 'Freestyle', 575.00, 40, 10)
+database.accessor.insert_product(cursor, str(
+    uuid.uuid4()), 'Verve', 'Trek Bikes', 'Electric', 3300.00, 15, 10)
+database.accessor.insert_product(cursor, str(
+    uuid.uuid4()), 'Slash', 'Trek Bikes', 'Trail', 12549.99, 5, 15)
+database.accessor.insert_product(cursor, str(
+    uuid.uuid4()), 'Propel Advanced', 'Giant Bicycle', 'Racing', 5200.00, 15, 10)
+database.accessor.insert_product(cursor, str(
+    uuid.uuid4()), 'Defy Advanced', 'Giant Bicycle', 'Endurance', 6300.00, 10, 10)
+database.accessor.insert_product(cursor, str(
+    uuid.uuid4()), 'RL 275', 'Redline', 'Big', 850.00, 20, 8)
+database.accessor.insert_product(cursor, str(
+    uuid.uuid4()), 'Asset 24', 'Redline', 'Freestyle', 575.00, 40, 10)
 
-#SALESPERSON SAMPLE
+# SALESPERSON SAMPLE
 # -------------------------------------------------------------------
-database.accessor.insert_salesperson(cursor, str(uuid.uuid4()), 'Jennifer', 'Henry', '2438 Beechwood Avenue Newark, NJ 07102', 9088660425, '04/12/2022', '06/12/2022')
-database.accessor.insert_salesperson(cursor, str(uuid.uuid4()), 'Lori', 'Holmes', '4653 Lynn Street Cambridge, MA 02141', 6172796409, '04/28/2018', '09/19/2022')
-database.accessor.insert_salesperson(cursor, str(uuid.uuid4()), 'Daniel', 'McCraken', '316 Terra Cotta Street Thief River Falls, MN 56701', 2186830606, '12/28/2019', '07/01/2022')
+database.accessor.insert_salesperson(cursor, str(uuid.uuid4(
+)), 'Jennifer', 'Henry', '2438 Beechwood Avenue Newark, NJ 07102', 9088660425, '04/12/2022', '06/12/2022')
+database.accessor.insert_salesperson(cursor, str(uuid.uuid4(
+)), 'Lori', 'Holmes', '4653 Lynn Street Cambridge, MA 02141', 6172796409, '04/28/2018', '09/19/2022')
+database.accessor.insert_salesperson(cursor, str(uuid.uuid4()), 'Daniel', 'McCraken',
+                                     '316 Terra Cotta Street Thief River Falls, MN 56701', 2186830606, '12/28/2019', '07/01/2022')
 
-#SALESPERSON SAMPLE
+# MANAGER SAMPLE
 # -------------------------------------------------------------------
 database.accessor.insert_manager(cursor, str(uuid.uuid4()), "Raymond", "Dix")
 database.accessor.insert_manager(cursor, str(uuid.uuid4()), "Erin", "Howell")
 
+# MANAGER SALESPERSONS SAMPLE
+# -------------------------------------------------------------------
+database.accessor.insert_manager_salespersons(
+    cursor, 'e4d3f660-5d40-4c1d-a58e-151cfcc55a2e', 'Raymond Dix')
+database.accessor.insert_manager_salespersons(
+    cursor, 'a3c2d1b5-a772-4602-9eb7-2b997528c23f', 'Raymond Dix')
+database.accessor.insert_manager_salespersons(
+    cursor, 'edef7d4c-9055-42ca-bb7c-2bc55d815b4e', 'Erin Howell')
+
+# PRINTS
+# -------------------------------------------------------------------
+products = database.accessor.get_products(cursor)
+managers = database.accessor.get_managers(cursor)
+salespersons = database.accessor.get_salespersons(cursor)
+salesperson_managers = database.accessor.get_manager_salespersons(cursor)
+
+print("products")
+for product in products:
+    print(product['name'])
+
+print("managers")
+for manager in managers:
+    print([manager['id'], manager['first_name'], manager['last_name']])
+
+print("salesperons")
+for salesperson in salespersons:
+    print([salesperson['id'], salesperson['first_name'], salesperson['last_name']])
+
+print("manager_salespersons")
+for row in salesperson_managers:
+    print([row["manager_id"], row["salesperson_id"]])
 
 conn.commit()
 cursor.close()
