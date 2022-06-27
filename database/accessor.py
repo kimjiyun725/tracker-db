b85f4b1ab1671917b520ab69572e3f7e0caacf29
kimjiyun725 <57276835+kimjiyun725@users.noreply.github.com>
2022-06-27 18:42:17 -0400

    Manager and Salesperson Cross References Properly (#2)

--- database/accessor.py
+++ database/accessor.py
@@ -7,7 +7,8 @@ def insert_product(cursor, id_, name, manufacturer, style, purchase_price, quant
             (id, name, manufacturer, style, purchase_price, quantity, commission_pct)
             VALUES(?, ?, ?, ?, ?, ?, ?)
             """,
-            (id_, name, manufacturer, style, purchase_price, quantity, commission_pct)
+            (id_, name, manufacturer, style,
+             purchase_price, quantity, commission_pct)
         )
 
         if not product_rows.lastrowid:
@@ -16,7 +17,8 @@ def insert_product(cursor, id_, name, manufacturer, style, purchase_price, quant
         return True
     except:
         return False
-    
+
+
 def get_products(cursor):
     try:
         cursor.execute("SELECT * FROM products")
@@ -26,6 +28,8 @@ def get_products(cursor):
         return {}
 
 # SALESPERSON FUNCTIONS
+
+
 def insert_salesperson(cursor, id_, first_name, last_name, address, phone, start_date, termination_date):
     try:
         salesperson_rows = cursor.execute(
@@ -34,7 +38,8 @@ def insert_salesperson(cursor, id_, first_name, last_name, address, phone, start
             (id, first_name, last_name, address, phone, start_date, termination_date)
             VALUES(?, ?, ?, ?, ?, ?, ?)
             """,
-            (id_, first_name, last_name, address, phone, start_date, termination_date)
+            (id_, first_name, last_name, address,
+             phone, start_date, termination_date)
         )
 
         if not salesperson_rows.lastrowid:
@@ -44,6 +49,7 @@ def insert_salesperson(cursor, id_, first_name, last_name, address, phone, start
     except:
         return False
 
+
 def get_salespersons(cursor):
     try:
         cursor.execute("SELECT * FROM salespersons")
@@ -52,7 +58,9 @@ def get_salespersons(cursor):
         print(e)
         return {}
 
-#MANAGER FUNCTIONS
+# MANAGER FUNCTIONS
+
+
 def insert_manager(cursor, id_, first_name, last_name):
     try:
         manager_rows = cursor.execute(
@@ -71,6 +79,7 @@ def insert_manager(cursor, id_, first_name, last_name):
     except:
         return False
 
+
 def get_managers(cursor):
     try:
         cursor.execute("SELECT * FROM managers")
@@ -79,10 +88,40 @@ def get_managers(cursor):
         print(e)
         return {}
 
-def check_managers(cursor, manager):
-    rows = get_managers(cursor)
-    for row in rows:
-        full_name = row['first_name'] + " " + row['last_name']
-        if full_name == manager:
-            return True
+# CROSS REFERENCE FUNCTIONS
+
+
+def insert_manager_salespersons(cursor, salesperson_id, manager_checker):
+    list_of_managers = get_managers(cursor)
+    # desired_manager_id = ''
+    for manager in list_of_managers:
+        manager_fullname = manager['first_name'] + " " + manager['last_name']
+        if manager_checker == manager_fullname:
+            # desired_manager_id = manager['id']
+
+            try:
+                manager_salespersons_rows = cursor.execute(
+                    """
+                    INSERT INTO manager_salespersons
+                    (manager_id, salesperson_id)
+                    VALUES(?, ?)
+                    """,
+                    (manager['id'], salesperson_id)
+                )
+                if not manager_salespersons_rows.lastrowid:
+                    return False
+
+                return True
+            except:
+                return False
+
     return False
+
+
+def get_manager_salespersons(cursor):
+    try:
+        cursor.execute("SELECT * FROM manager_salespersons")
+        return cursor.fetchall()
+    except Exception as e:
+        print(e)
+        return {}
